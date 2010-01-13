ab2415d84da801052055c9f2ec66c714a6115b44
Oleg Ivanov <morhekil@morhekil.net>
2010-01-13 21:29:03 +0200

    ftpsync-based operations for syncing over FTP, minor cleanup

--- lib/prisync.rb
+++ lib/prisync.rb
@@ -16,12 +16,12 @@ class Prisync
   # Starts the synchronization process, options are
   #   :local_path - local path to watch for changes,
   #   :target_path - target (remote) path to sync with,
-  #   :ssh_port - SSH port to use if syncing remotely over the ssh tunnel
+  #   :port - port to use if rsyncing remotely over ssh tunnel
   #   :timeout - seconds to wait for any new changes to arrive before starting the actual synchronization
   #   :exclude - regexp for excluding files, if omitted or nil - no excluding,
   #   :sync_on_start - trigger synchronization on start if set,
   #   :dryrun - run rsync with --dry-run option to not perform any actual changes if set,
-  #   :debug - print out extra debug information if set
+  #   :debug - $stdout.puts out extra debug information if set
   def start(opts)
     
     # init runtime parameters
@@ -30,12 +30,22 @@ class Prisync
     @timeout = opts[:timeout] || DEFAULT_TIMEOUT
     @is_debug = opts[:debug]
     @is_dryrun = opts[:dryrun]
-    # build the rsync command
-    @rsync_cmd = 'rsync -azcC --force --delete '
-    @rsync_cmd += '--dry-run ' if opts[:dryrun]
-    @rsync_cmd += '--progress ' if @is_debug
-    @rsync_cmd += "-e \"ssh -p#{opts[:ssh_port]}\" " if opts[:ssh_port]
-    @rsync_cmd += "#{opts[:local_path]} #{opts[:remote_path]}"
+    
+    # build the sync command
+    if opts[:mode] == :rsync
+      # rsync mode
+      @sync_cmd = 'rsync -azcC --force --delete '
+      @sync_cmd += '--dry-run ' if opts[:dryrun]
+      @sync_cmd += '--progress ' if @is_debug
+      @sync_cmd += "-e \"ssh -p#{opts[:port]}\" " if opts[:port]
+      @sync_cmd += "#{opts[:local_path]} #{opts[:remote_path]}"
+    else
+      # ftpsync mode
+      @sync_cmd = 'ftpsync.pl -p '
+      @sync_cmd += '-i ' if opts[:dryrun]
+      @sync_cmd += '-v ' if @is_debug
+      @sync_cmd += "#{opts[:local_path]} #{opts[:remote_path]}"
+    end
 
     # Start inotify watcher
     @inotify.watch_dir_recursively(opts[:local_path], @inotify_mask)
@@ -44,13 +54,13 @@ class Prisync
       Signal.trap(signal_name, proc { self.cleanup })
     end
 
-    print "Started synchronization with the following options:\n#{opts.pretty_inspect}\n\n" if @is_debug
+    $stdout.puts "Started synchronization with the following options:\n#{opts.pretty_inspect}\n\n" if @is_debug
     
     while (true) do
       begin
         self.monitor_changes
       rescue
-        print "Exception: ",$!, "\n"
+        $stderr.puts "Exception: ",$!, "\n"
       end
     end
 
@@ -61,38 +71,38 @@ class Prisync
     # Check to see if we've got any new changes
     new_changes = false
     while ( (events = @inotify.next_events(true)).size > 0 ) do
-      print "Detected #{events.size} new events\n" if @is_debug
+      $stdout.puts "Detected #{events.size} new events\n" if @is_debug
       new_changes = true if events.find { |event| @exclude.nil? || event.filename !~ @exclude }
-      print "Has synchronizable changes\n" if new_changes && @is_debug
+      $stdout.puts "Has synchronizable changes\n" if new_changes && @is_debug
     end
     
     # Check if we've got any pending changes those need to be synced and timeout has passed indicating that we're ready to go
     if (@pending_changes && !new_changes) then
-      print "Synchronizing...\n" if @is_debug
+      $stdout.puts "Synchronizing...\n" if @is_debug
       self.synchronize
       @pending_changes = false
     end
     
     # Update pending status and wait for a timeout
     @pending_changes = true if new_changes
-    print "Pending changes\n" if @pending_changes && @is_debug
+    $stdout.puts "Pending changes\n" if @pending_changes && @is_debug
     sleep @timeout
       
   end
 
-  # Actually synchronizes the files by calling rsync using the pre-built command line
+  # Actually synchronizes the files by calling the pre-built command
   def synchronize
-    print "#{@rsync_cmd}\n" if @is_debug
-    system @rsync_cmd
+    $stdout.puts "#{@sync_cmd}\n" if @is_debug
+    system @sync_cmd
     errorvalue = $?
     errorvalue = errorvalue.exitstatus if errorvalue.kind_of?(Process::Status)
-    $stderr.print "rsync error status #{errorvalue}\n" if @is_debug
+    $stdout.puts "rsync error status #{errorvalue}\n" if @is_debug
     self.cleanup if errorvalue == 20 || errorvalue == 255
   end
 
   # Cleans up before exit, closing inotify connection
   def cleanup
-    print "Cleaning up...\n" if @is_debug
+    $stdout.puts "Cleaning up...\n" if @is_debug
     @inotify.close
     exit 0
   end
--- prisync.gemspec
+++ prisync.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'prisync'
-  s.version = "0.2.1"
-  s.date     = "2008-10-01"
+  s.version = "0.2.2"
+  s.date     = "2010-01-13"
   s.summary = "Pseudo-realtime file synchronization daemon."
   s.description = %{Prisync allows to synchronize to local or remote directories in a pseudo-realtime fashion.
-  Filesystem changes are monitored with inotify kernel events, rsync utility is used for the actual
-  synchronization and can be run over SSH.%}
+  Filesystem changes are monitored with inotify kernel events, rsync or ftpsync utilities are used for the actual
+  synchronization. Rsync can be run over SSH.%}
   s.has_rdoc = false
   
   s.files = ['lib/inotify.rb',
@@ -22,5 +22,5 @@ Gem::Specification.new do |s|
 
   s.author = "Oleg Ivanov"
   s.email = "morhekil@morhekil.net"
-  s.homepage = "http://morhekil.net"
+  s.homepage = "http://speakmy.name"
 end
